614d5650463f7a41f4bd220cab35e7dd2c4c9bea
Syed Zeeshan Ahmed <160160824+syedzeeshanahmed27@users.noreply.github.com>
2025-01-23 18:51:55 -0500

    Update loader.js

--- script/loader.js
+++ script/loader.js
@@ -307,6 +307,7 @@ var nav_bar_code = `<nav>
             <a href="tel:(973) 3458-1142">+973 3458-1142 </a>
         <a>BCCI Tower 2nd Floor, Office number 21, Building number 519, Road 1010, Block 410, Manama - kingdom of Bahrain</a>
 <a href="tel:(966) 53529-5011">+966 523529-5011 </a>
+<br></br>
             <a>0l Olaya, Riyadh 12212, Saudi Arabia</a>
         </li>         
         <li class="link_text">
