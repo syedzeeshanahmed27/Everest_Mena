799ccc120d72af33fd197391c2574889b8bf0127
Syed Zeeshan Ahmed <160160824+syedzeeshanahmed27@users.noreply.github.com>
2026-01-10 17:54:27 +0500

    new change

--- script/loader.js
+++ script/loader.js
@@ -317,11 +317,12 @@ var nav_bar_code = `<nav>
     font-weight: 600;
 ">Middle East : KSA, Bahrain Qatar & UAE</a>
         
-            <a href="tel:(973) 3458-1142">+973 3458-1142 </a>
-        <a>BCCI Tower 2nd Floor, Office number 21, Building number 519, Road 1010, Block 410, Manama - kingdom of Bahrain</a>
-        <br>
-<a href="tel:(966) 53529-5011">+966 50 658 0506 </a>
+            <a href="tel:(966) 53529-5011">+966 50 658 0506 </a>
             <a>Building No: 7135, King Abdul Aziz Street, Al Arid District, Postal Code: 13342, Riyadh, Kingdom of Saudi Arabia</a>
+        <br>
+<a href="tel:(973) 3458-1142">+973 3458-1142 </a>
+        <a>BCCI Tower 2nd Floor, Office number 21, Building number 519, Road 1010, Block 410, Manama - kingdom of Bahrain</a>
+
         </li>         
         <li class="link_text">
             <a style="
@@ -345,6 +346,7 @@ var nav_bar_code = `<nav>
 
 document.getElementById("navbar").innerHTML = nav_bar_code;
 document.getElementById("footer_content").innerHTML = footer_code;
+
 
 
 
